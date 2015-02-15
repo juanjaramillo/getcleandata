4cac2ce97ed1d3b04d9849b02df6804beb9a6693
juanjaramillo <juanjaramillo855@gmail.com>
2015-02-15 10:46:44 -0500

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,15 +1,16 @@
 
-
+## Prepare the data 
 temp <- tempfile()
 download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip",temp,method="curl")
 lsfls <- unzip(temp)
 unlink(temp)
+## Remove some files that does not have important information for the task
 fls <- lsfls[-3]
 fls <- fls[-3]
 length(fls)
 
 ## 1. Merges the training and the test sets to create one data set.
-## What I think I have to do here is to attach cbind(X_test,X_train) and also cbind(y_test,y_train)
+## Attach rbind(X_test,X_train) and also rbind(y_test,y_train) 
 ## and merge both of them, and give them the names of the features 
 
 Xtest <- read.table(fls[13])
@@ -19,14 +20,15 @@ ytrain <- read.table(fls[26])
 
 Xmerge <- rbind(Xtest,Xtrain)
 ymerge <- rbind(ytest,ytrain)
-tidy1 <- cbind(ymerge,Xmerge) # Base de datos que contiene las variables independientes y los features
+tidy1 <- cbind(ymerge,Xmerge) # Database that has the y's and the features
 feat <- read.table(fls[2])
 names <- as.character(feat[,2])
 colnames(tidy1) <- c("Labels", names)
 colnames(tidy1)
 
 ## 2. Extracts only the measurements on the mean and standard deviation for each measurement.
-## 
+ 
+## Extract the variables that have Mean and Standar Deviation on iis name
 newnames <- ""
 for(i in 1:length(names)){
   if(grepl("mean",names[i])||grepl("std",names[i])){
@@ -42,20 +44,19 @@ for(i in 1:length(newnames)){
 }
 
 ## 3. Uses descriptive activity names to name the activities in the data set
-## 
+
 labels <- read.table(fls[1]) ## where the activity_labels.txt is.
 ActivityNames <- rep("",dim(tidy2)[1])
-
 for(i in 1:length(tidy2[,1])){
   ActivityNames <- as.character(labels[tidy2[,i],2])
 }
+## Add it as a column to the data set.
 tidy2[,1] <- ActivityNames
 
 
 ## 4. Appropriately labels the data set with descriptive variable names. 
-##
 desnames <- newnames
-
+# Change the shorts for Long Names and CamelCase
 for(i in 1:length(desnames)){
   desnames[i] <- gsub("-std","SDev",desnames[i])
   if(noquote(strsplit(desnames[i], NULL)[[1]])[1]=='t'){
@@ -101,7 +102,9 @@ tidy5[,1] <- krass[,80]
 tidy5[,2] <- krass[,81]
 for(i in 1:(dim(krass)[2]-2)){
   tidy5[,i+2] <- krass[,i]
-}
+} 
+# Give the proper names to each of the columns-variables 
 colnames(tidy5) <- c("Subject", "Activity",colnames(krass)[1:(dim(krass)[2]-2)])
 View(tidy5)
-write.table(tidy5,file="./Desktop/TidyDataSet.txt",row.name=FALSE)
+# Write the file with the tidy data set 
+write.table(tidy5,file="TidyDataSet.txt",row.name=FALSE)
